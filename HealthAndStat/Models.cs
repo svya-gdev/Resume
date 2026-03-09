68d4de9c747a3b7ee53b3663d89814986045dd69
svya <svya.gamedeveloper@gmail.com>
2026-03-10 00:24:09 +0300

    Add project files.

---
+++ HealthAndStat/Models.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+
+namespace ResumeGame.Models;
+
+public readonly record struct Buff<T>(T Amount)
+    where T : INumber<T>, IUnsignedNumber<T>, IMinMaxValue<T>;
+public readonly record struct Nerf<T>(T Amount)
+    where T : INumber<T>, IUnsignedNumber<T>, IMinMaxValue<T>;
+
+public readonly record struct Harm<T>(T Amount)
+    where T : INumber<T>, IUnsignedNumber<T>, IMinMaxValue<T>;
+public readonly record struct Heal<T>(T Amount)
+    where T : INumber<T>, IUnsignedNumber<T>, IMinMaxValue<T>;
+
+public enum DamageTypes : byte { None, Bludgeoning, Piercing, Slashing }
+public readonly record struct Damage<T>(Harm<T> Harm, DamageTypes Type)
+    where T : INumber<T>, IUnsignedNumber<T>, IMinMaxValue<T>;
+
+public readonly record struct Stat<T>
+    where T : INumber<T>, IUnsignedNumber<T>, IMinMaxValue<T>
+{
+    public T Minimum { get; }
+    public T Current { get; }
+    public T Maximum => T.MaxValue;
+    public Stat() => (Minimum, Current) = (T.MinValue, T.MinValue);
+    public Stat(T minimum) => (Minimum, Current) = (minimum, minimum);
+    public Stat(T minimum, T current) => (Minimum, Current) = (minimum, T.Max(minimum, current));
+}
+
+public readonly record struct Health<T>
+    where T : INumber<T>, IUnsignedNumber<T>, IMinMaxValue<T>
+{
+    public T Minimum => T.MinValue;
+    public T Current { get; }
+    public T Maximum { get; }
+    public Health() => (Current, Maximum) = (T.MaxValue, T.MaxValue);
+    public Health(T maximum) => (Current, Maximum) = (maximum, maximum);
+    public Health(T current, T maximum) => (Current, Maximum) = (T.Min(current, maximum), maximum);
+}
---
+++ HealthAndStat/Usage.cs
@@ -0,0 +1,56 @@
+﻿using System.Numerics;
+
+namespace ResumeGame.Models.Usage;
+
+public static class Application
+{
+    extension<T>(Stat<T> stat)
+        where T : INumber<T>, IUnsignedNumber<T>, IMinMaxValue<T>
+    {
+        public Stat<T> Apply(Buff<T> buff)
+        {
+            var min = stat.Minimum;
+            var cur = buff.Amount < T.MaxValue - stat.Current ? stat.Current + buff.Amount : T.MaxValue;
+            return new(min, cur);
+        }
+
+        public Stat<T> Apply(Nerf<T> nerf)
+        {
+            var min = stat.Minimum;
+            var cur = nerf.Amount < stat.Current ? stat.Current - nerf.Amount : stat.Minimum;
+            return new(min, cur);
+        }
+    }
+
+    extension<T>(Health<T> health)
+        where T : INumber<T>, IUnsignedNumber<T>, IMinMaxValue<T>
+    {
+        public Health<T> Apply(Harm<T> harm)
+        {
+            var cur = harm.Amount < health.Current ? health.Current - harm.Amount : health.Minimum;
+            var max = health.Maximum;
+            return new(cur, max);
+        }
+
+        public Health<T> Apply(Heal<T> heal)
+        {
+            var cur = heal.Amount < health.Maximum - health.Current ? health.Current + heal.Amount : health.Maximum;
+            var max = health.Maximum;
+            return new(cur, max);
+        }
+
+        public Health<T> Apply(Buff<T> buff)
+        {
+            var cur = health.Current;
+            var max = buff.Amount < T.MaxValue - health.Maximum ? health.Maximum + buff.Amount : T.MaxValue;
+            return new(cur, max);
+        }
+
+        public Health<T> Apply(Nerf<T> nerf)
+        {
+            var cur = health.Current;
+            var max = nerf.Amount < health.Maximum ? health.Maximum - nerf.Amount : health.Minimum;
+            return new(cur, max);
+        }
+    }
+}
